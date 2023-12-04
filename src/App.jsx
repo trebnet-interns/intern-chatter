8c9768100a3251e20324a08e132c5a55cbcaae35
Larry-lingston <theodoreagbozo87@gmail.com>
2023-12-04 08:55:47 +0000

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@
 import { useState, } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import GridLoader from "react-spinners/GridLoader";
-import './App.css'
+import GridLoader from "react-spinners/GridLoader"; 
+ const [loading, setLoading] = useState(true);
+ import ProfilePage from "./Components/profilepage";
 
-function App() {
-  const [loading, setLoading] = useState(true);
+ function App() {
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className="App">
 
-        <div className="item">
+        {/* <div className="item">
           <GridLoader
           loading={loading}
           size={13}
           color="blue"
           />
-        </div>
+        </div> */}
+
+        <ProfilePage />
     </div>
   );
 }
